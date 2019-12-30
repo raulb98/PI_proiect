e4d57be8144195d797d4c99f1cfb2a54235a52a5
raulb98 <52500444+raulb98@users.noreply.github.com>
2019-12-30 14:14:27 +0200

    Update criptare.cpp

--- criptare.cpp
+++ criptare.cpp
@@ -7,75 +7,85 @@ typedef unsigned char BYTE;
 typedef unsigned long DWORD;
 
 
-int main()
+int main(int argc,char* argv[])
 {
 	cv::Mat img;
 
-	img = cv::imread("lena.png", 1);
-	std::string mesaj;
-	std::cout << "Introduceti mesaj : ";
-	std::getline(std::cin,mesaj);
-
-	BYTE MASK = 0x80;
-	DWORD y = 0;
-	DWORD x = 0;
-	for (auto elem : mesaj)
+	img = cv::imread(argv[2], 1);
+	if (argc > 1)
 	{
-		DWORD counter = 8;
-		while (counter)
+		std::string mesaj{ argv[1] };
+		
+		std::cout << argv[2];
+		std::cout << mesaj;
+
+		BYTE MASK = 0x80;
+		DWORD y = 0;
+		DWORD x = 0;
+		for (auto elem : mesaj)
 		{
-			BYTE bit_of_interest = elem & MASK;
-			bit_of_interest = bit_of_interest >> (counter - 1);
-			MASK = MASK >> 1;
-			counter--;
-
-			if (x + 3 < img.cols)
+			DWORD counter = 8;
+			while (counter)
 			{
-				BYTE& oct1 = img.at<BYTE>(y, x);
-				BYTE& oct2 = img.at<BYTE>(y, x + 1);
-				BYTE& oct3 = img.at<BYTE>(y, x + 2);
+				BYTE bit_of_interest = elem & MASK;
+				bit_of_interest = bit_of_interest >> (counter - 1);
+				MASK = MASK >> 1;
+				counter--;
 
-				oct1 = ((oct1 >> 1) << 1) | (bit_of_interest);
-				oct2 = ((oct2 >> 1) << 1) | (bit_of_interest);
-				oct3 = ((oct3 >> 1) << 1) | (bit_of_interest);
+				if (x + 3 <= 3 * img.cols)
+				{
+					BYTE& oct1 = img.at<BYTE>(y, x);
+					BYTE& oct2 = img.at<BYTE>(y, x + 1);
+					BYTE& oct3 = img.at<BYTE>(y, x + 2);
 
-				x += 3;
+					oct1 = ((oct1 >> 1) << 1) | (bit_of_interest);
+					oct2 = ((oct2 >> 1) << 1) | (bit_of_interest);
+					oct3 = ((oct3 >> 1) << 1) | (bit_of_interest);
+
+					x += 3;
+				}
+				else
+				{
+					x = 0;
+					y++;
+
+					BYTE& oct1 = img.at<BYTE>(y, x);
+					BYTE& oct2 = img.at<BYTE>(y, x + 1);
+					BYTE& oct3 = img.at<BYTE>(y, x + 2);
+
+					oct1 = ((oct1 >> 1) << 1) | (bit_of_interest);
+					oct2 = ((oct2 >> 1) << 1) | (bit_of_interest);
+					oct3 = ((oct3 >> 1) << 1) | (bit_of_interest);
+
+					x += 3;
+				}
+
 			}
-			else
-			{
-				x = 0;
-				y++;
-
-				BYTE& oct1 = img.at<BYTE>(y, x);
-				BYTE& oct2 = img.at<BYTE>(y, x + 1);
-				BYTE& oct3 = img.at<BYTE>(y, x + 2);
-
-				oct1 = ((oct1 >> 1) << 1) | (bit_of_interest);
-				oct2 = ((oct2 >> 1) << 1) | (bit_of_interest);
-				oct3 = ((oct3 >> 1) << 1) | (bit_of_interest);
-
-				x += 3;
-			}
-
+			MASK = 0x80;
 		}
-		MASK = 0x80;
+
+		if (x < 3 * img.cols)
+		{
+			BYTE& oct1 = img.at<BYTE>(y, x);
+			BYTE& oct2 = img.at<BYTE>(y, x + 1);
+			BYTE& oct3 = img.at<BYTE>(y, x + 2);
+
+			oct1 = 'R';
+			oct2 = 'C';
+			oct3 = 'T';
+		}
+
 	}
+	cv::imwrite("D:\\Mesaj.png", img);
+	/*std::cout << (int)img.at<BYTE>(0, 669) << " " << (int)img.at<BYTE>(0, 670) << " " << (int)img.at<BYTE>(0, 671) << " ";
+	std::cout << (int)img.at<BYTE>(0, 672) << " " << (int)img.at<BYTE>(0,673 ) << " " << (int)img.at<BYTE>(0, 674) << " ";
+	for (int i = 0; i < 8 * 3; ++i) {
+		std::cout<< (int)img.at<BYTE>(1, i) << " ";
+	}*/
+	std::cout << "\n";
+	//cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); // Create a window for display.
+	//cv::imshow("Display window", img);
 
-	if (x < img.cols)
-	{
-		BYTE& oct1 = img.at<BYTE>(y, x);
-		BYTE& oct2 = img.at<BYTE>(y, x + 1);
-		BYTE& oct3 = img.at<BYTE>(y, x + 2);
-
-		oct1 = 'R';
-		oct2 = 'C';
-		oct3 = 'T';
-	}
-
-	cv::imwrite("C:\\Users\\rbucur\\source\\repos\\Project10\\Project10\\decriptare.png", img);
-
-	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); // Create a window for display.
-	cv::imshow("Display window", img);
 	cv::waitKey(0);
 	return 0;
 }
