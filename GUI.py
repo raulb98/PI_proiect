08f05d11aafe685682b44d2bf414e9f7ee23bea1
raulb98 <52500444+raulb98@users.noreply.github.com>
2020-01-09 12:27:00 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,13 +3,40 @@ from tkinter import filedialog
 import subprocess
 from functools import partial
 from PIL import Image,ImageTk
+from os import path
 
 root = Tk()
 root.geometry("500x500+450+150")
 
 def Crypt_process():
     msg_to_crypt=entry_1.get()
-    subprocess.call([r'C:\Users\rbucur\source\repos\Pi_Proiect_Criptare\x64\Debug\Pi_Proiect_Criptare.exe',msg_to_crypt,img_path])
+    try:
+        subprocess.call([r'C:\Users\rbucur\source\repos\Pi_Proiect_Criptare\x64\Debug\Pi_Proiect_Criptare.exe',msg_to_crypt,img_path])
+    except Exception as e:
+        print(e)
+        return
+
+    fav = len(msg_to_crypt) * 24 + 3
+    total = path.getsize(img_path)
+
+    label_size = Label(root, text='Text Length:')
+    label_size.grid(row=6, column=0)
+    entry_size = Entry(root)
+    entry_size.insert(0, len(msg_to_crypt))
+    entry_size.grid(row=6, column=1)
+
+    label_bytes = Label(root, text='Bytes changed:')
+    label_bytes.grid(row=7, column=0)
+    entry_bytes = Entry(root)
+    entry_bytes.insert(0, fav)
+    entry_bytes.grid(row=7, column=1)
+
+    label_integrity = Label(root, text='Image Integrity:')
+    label_integrity.grid(row=8, column=0)
+    entry_integrity = Entry(root)
+    entry_integrity.insert(0, (1-fav/total)*100)
+    entry_integrity.grid(row=8, column=1)
+
 
 def Decrypt_process():
     entry_2.delete(0,'end')
@@ -26,19 +53,28 @@ def Browse_picture():
     render = ImageTk.PhotoImage(load)
     image = Label(root,image=render)
     image.image = render
-    image.grid(row=4,column=0,columnspan=2,rowspan=2)
+    image.grid(row=4,column=0)
+
+    entry_path = Entry(root)
+    entry_path.insert(0,root.filename)
+    entry_path.grid(row=2,column=1)
+
+    label_size = Label(root,text='Image Size:')
+    label_size.grid(row=5,column=0)
+    entry_size = Entry(root)
+    entry_size.insert(0, path.getsize(root.filename))
+    entry_size.grid(row=5,column=1)
 
 message_to_crypt=StringVar()
-label1 = Label(root, text="Criptare: ")
 entry_1 = Entry(root,textvariable=message_to_crypt)
-
-
-label2 = Label(root, text="Decriptare: ")
 entry_2 = Entry(root)
 
 cryptButton = Button(root, text="Crypting", fg="black",command=Crypt_process)
+cryptButton.config(width=34)
 decryptButton = Button(root, text="Decrypting", fg="black",command=Decrypt_process)
+decryptButton.config(width=34)
 pathButton = Button(root,text="Path to file", fg="black",command=Browse_picture)
+pathButton.config(width=34)
 decryptButton.grid(row=1,sticky=W)
 cryptButton.grid(row=0,sticky=W)
 pathButton.grid(row=2,sticky=W)
